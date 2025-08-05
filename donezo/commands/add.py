d7e9cd0c6a6fdf7249ec3a0f54143fdddc70e81c
Kavan Samra <kavansamra@gmail.com>
2025-08-05 13:18:05 -0700

    basic add functionality. Serialization of Task

--- donezo/commands/add.py
+++ donezo/commands/add.py
@@ -1,4 +1,35 @@
 from donezo.models.task import Task
+from donezo.storage import getDataPath
+import json
 
 def add(desc: str):
-    print(f'received new task to track! {desc=}')
+    data_path = getDataPath()
+
+    try:
+        with open(data_path, "r") as f:
+            data = json.load(f)
+            print(f'{data=}')
+    except FileNotFoundError:
+        data = {}
+
+
+    if data:
+        task_id = data['latest_id']
+        task_id += 1
+        data['latest_id'] = task_id
+        new_task = Task(task_id, desc)
+        data['tasks'].append(new_task.to_dict())
+
+    else:
+        data['latest_id'] = 0
+        new_task = Task(0, desc)
+        data['tasks'] = []
+        data['tasks'].append(new_task.to_dict())
+
+    
+    with open(data_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=2)
+
+
+if __name__ == '__main__':
+    add('My first task!')
--- donezo/models/task.py
+++ donezo/models/task.py
@@ -1,7 +1,35 @@
+import datetime
 class Task:
-    def __init__(self):
-        self.id = 1 #auto-incrementing
-        self.description = 'test'
-        self.status = 'test'
-        self.createdAt = 'test' #datetime
-        self.updatedAt = 'test' #datetime
+    def __init__(self, id: int, desc: str):
+        self.id = id
+        self.description = desc
+        self.status = 'todo'
+        self.createdAt = datetime.datetime.now(datetime.timezone.utc)
+        self.updatedAt: datetime.datetime | None = None
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "status": self.status,
+            "createdAt": self.createdAt.isoformat(),
+            "updatedAt": self.updatedAt.isoformat() if self.updatedAt else None
+        }
+    
+if __name__ == '__main__':
+
+    #testing Task serialization/deserialization
+    test_task = Task(0, 'testing')
+
+    #json serialization
+    task_dict = test_task.to_dict()
+    print(f'{task_dict=}')
+    import json
+    task_json = json.dumps(task_dict)
+    print(f'{task_json=}')
+
+    #json deserialization
+    print('deserializing')
+    task_dict = json.loads(task_json)
+    print(f'{task_dict=}')
+    
