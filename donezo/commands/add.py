52bb6cb4040f71edebb61071371cc1d9c1386698
Kavan Samra <kavansamra@gmail.com>
2025-08-07 12:52:02 -0700

    changed json structure

--- donezo/commands/add.py
+++ donezo/commands/add.py
@@ -2,34 +2,40 @@ from donezo.models.task import Task
 from donezo.storage import getDataPath
 import json
 
-def add(desc: str):
+def add(desc: str) -> int:
+    """
+    Add a new task with the provided desc to be tracked
+
+    Returns:
+        int: the id of the newly created task
+    """
     data_path = getDataPath()
 
     try:
         with open(data_path, "r") as f:
             data = json.load(f)
-            print(f'{data=}')
     except FileNotFoundError:
         data = {}
 
-
+    task_id = 0
     if data:
         task_id = data['latest_id']
         task_id += 1
         data['latest_id'] = task_id
-        new_task = Task(task_id, desc)
-        data['tasks'].append(new_task.to_dict())
-
+        
     else:
-        data['latest_id'] = 0
-        new_task = Task(0, desc)
-        data['tasks'] = []
-        data['tasks'].append(new_task.to_dict())
+        data['latest_id'] = task_id
 
-    
+    new_task = Task(task_id, desc)
+    data['tasks'][str(task_id)] = new_task.to_dict()
+
     with open(data_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2)
 
+    return task_id
+
 
 if __name__ == '__main__':
-    add('My first task!')
+    id = add('My first task!')
+    print(f'created new task with {id=}')
+    
